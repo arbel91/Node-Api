88ab9b8f47d3eaa9a80d15fbdb9a7e6393e419da
Harold <andresdias24@gmail.com>
2021-04-03 17:45:47 -0500

    add rout post

--- src/routers/movie.js
+++ src/routers/movie.js
@@ -1,12 +1,21 @@
 const { Router } = require('express');
 const router = Router()
+const dataMovies = require('./sample.json')
 
 router.get('/', (req, res) => {
-    const data = {
-        "name" : "harold",
-        "apellido": "diaz"
-    }
-    res.json(data)
+    res.json(dataMovies)
 })
 
+router.post('/', (req, res) => {
+    const {title, director, year, rating} = req.body
+    if (title && director && year && rating) {
+        const id = dataMovies.length + 1
+        const newMocies = {...req.body, id}
+        dataMovies.push(newMocies)
+        res.json(dataMovies)
+    } else {
+        res.send('wrong req')
+    }
+
+})
 module.exports = router
--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan')
 const routerHome = require('./routers/home')
-const routerMovie = require('./routers/home')
+const routerMovie = require('./routers/movie')
 // setings
 app.set('port', process.env.PORT || 3000)
 
