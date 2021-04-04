5b02afd762ebcc50ec1273a08d2e4de4a0458d79
Harold <andresdias24@gmail.com>
2021-04-03 19:19:40 -0500

    add router delete

--- src/routers/movie.js
+++ src/routers/movie.js
@@ -1,21 +1,35 @@
 const { Router } = require('express');
 const router = Router()
-const dataMovies = require('./sample.json')
+const movies = require('./sample.json')
+const _ = require('underscore')
+
 
 router.get('/', (req, res) => {
-    res.json(dataMovies)
+    res.json(movies)
 })
 
 router.post('/', (req, res) => {
     const {title, director, year, rating} = req.body
     if (title && director && year && rating) {
-        const id = dataMovies.length + 1
-        const newMocies = {...req.body, id}
-        dataMovies.push(newMocies)
-        res.json(dataMovies)
+        const _id = movies.length + 1
+        const newMovies = { _id, ...req.body}
+        movies.push(newMovies)
+        res.json(movies)
     } else {
         res.send('wrong req')
     }
-
 })
+
+router.delete('/:id', (req, res) => {
+    const {id} = req.params;
+    if (id) {
+        _.each(movies, (movie, i) => {
+            const _id = movie._id
+            if (_id == id) {
+                movies.splice(i, 1);
+            }
+        });
+        res.json(movies);
+    }
+});
 module.exports = router
